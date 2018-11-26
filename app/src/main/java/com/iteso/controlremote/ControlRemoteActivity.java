f41b7c1d1dd58d22518cdcc70bea7c72ef01389f
JuanCarlos OrtizPinto <juancarlos_tecno91@hotmail.com>
2018-11-25 21:31:06 -0600

    Bluetooth connection

--- app/src/main/java/com/iteso/controlremote/ControlRemoteActivity.java
+++ app/src/main/java/com/iteso/controlremote/ControlRemoteActivity.java
@@ -1,13 +1,21 @@
 package com.iteso.controlremote;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.iteso.controlremote.source.ConnectThread;
 import com.iteso.controlremote.source.Tools;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ControlRemoteActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,6 +31,21 @@ public class ControlRemoteActivity extends AppCompatActivity implements View.OnC
         ibIzquierda = findViewById(R.id.ibIzquierda);
         ibDetener = findViewById(R.id.ibDetener);
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            Tools.NotificacionToast(this,"Bluetooth no disponible", Toast.LENGTH_LONG);
+        } else{
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+            }else{
+                mBluetoothDevice = mBluetoothAdapter.getRemoteDevice(SERVICE_ADDRESS);
+                ConnectThread connect = new ConnectThread(mBluetoothDevice,SERVICE_ID,mBluetoothAdapter);
+                connect.start();
+                mBluetoothSocket = connect.getOutSocket();
+            }
+        }
+
         ibLuz.setOnClickListener(this);
         ibClaxon.setOnClickListener(this);
         ibDerecha.setOnClickListener(this);
@@ -36,48 +59,53 @@ public class ControlRemoteActivity extends AppCompatActivity implements View.OnC
         switch (v.getId()){
             case R.id.ibLuz:
                 Accion = 4;
-                Tools.NotificacionToast(this,"Luces");
+                Tools.NotificacionToast(this,"Luces",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
             case R.id.ibClaxon:
                 Accion = 5;
-                Tools.NotificacionToast(this,"Claxón");
+                Tools.NotificacionToast(this,"Claxón",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
             case R.id.ibDerecha:
                 Accion = 3;
-                Tools.NotificacionToast(this,"Derecha");
+                Tools.NotificacionToast(this,"Derecha",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
             case R.id.ibAvanza:
                 Accion = 1;
-                Tools.NotificacionToast(this,"Avanzar");
+                Tools.NotificacionToast(this,"Avanzar",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
             case R.id.ibIzquierda:
                 Accion = 2;
-                Tools.NotificacionToast(this,"Izquierda");
+                Tools.NotificacionToast(this,"Izquierda",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
             case R.id.ibDetener:
                 Accion = 0;
-                Tools.NotificacionToast(this,"Detener");
+                Tools.NotificacionToast(this,"Detener",Toast.LENGTH_SHORT);
                 TxSendData(Accion);
                 break;
         }
     }
     private void TxSendData(int DataInformation){
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
-        }else{
-            Intent SendData = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(SendData,DataInformation);
+        if(mBluetoothSocket != null){
+            try{
+                OutputStream outData = mBluetoothSocket.getOutputStream();
+                outData.write(DataInformation);
+            }catch (IOException ex){
+                ex.getStackTrace();
+            }
         }
     }
 
     private ImageButton ibLuz, ibClaxon, ibDerecha, ibAvanza, ibIzquierda, ibDetener;
+    public static final String SERVICE_ID = "00001101-0000-1000-8000-00805f9b34fb"; //SPP UUID
+    public static final String SERVICE_ADDRESS = "98:D3:33:80:B2:DB"; // HC-05 BT ADDRESS
     private final static int REQUEST_ENABLE_BT = 1;
+    private BluetoothSocket mBluetoothSocket = null;
+    private BluetoothDevice mBluetoothDevice = null;
+    private BluetoothAdapter mBluetoothAdapter = null;
     private static int Accion;
 }
